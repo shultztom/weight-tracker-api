9aeb24bee65774dab7e757eb9f772d5aa5f57f6e
shultztom <shultzthomask@sau.edu>
2022-07-26 19:05:16 -0500

    Added age and rest of base user routes

--- src/main/java/com/shultzlab/weighttrackerapi/controllers/UserController.java
+++ src/main/java/com/shultzlab/weighttrackerapi/controllers/UserController.java
@@ -1,8 +1,14 @@
 package com.shultzlab.weighttrackerapi.controllers;
 
+import com.shultzlab.weighttrackerapi.exceptions.ResourceNotFoundException;
 import com.shultzlab.weighttrackerapi.models.User;
 import com.shultzlab.weighttrackerapi.repositories.UserRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @CrossOrigin
 @RestController
@@ -15,8 +21,14 @@ public class UserController {
     }
 
     @GetMapping()
-    public String getAllUsers(){
-        return "YO";
+    public List<User> getAllUsers(){
+        return this.userRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable(value = "id") Long userId) throws ResourceNotFoundException {
+        User user = this.userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User not found with id: " + userId));
+        return ResponseEntity.ok().body(user);
     }
 
     @PostMapping
@@ -24,6 +36,26 @@ public class UserController {
         // TODO ensure user.username matches username in token
         // TODO validate
         return this.userRepository.save(user);
+    }
+
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable(value = "id") Long userId, @RequestBody User userRequest) throws ResourceNotFoundException {
+        User user = this.userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User not found with id: " + userId));
+        user.setUsername(userRequest.getUsername());
+        user.setHeight(userRequest.getHeight());
+        user.setBirthday(userRequest.getBirthday());
+        user.setActivityLevel(userRequest.getActivityLevel());
+        user.setGender(userRequest.getGender());
+        return this.userRepository.save(user);
+    }
+
+    @DeleteMapping("/{id}")
+    public Map<String, Boolean> deleteUser(@PathVariable(value = "id") Long userId) throws ResourceNotFoundException{
+        User user = this.userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User not found with id: " + userId));
+        this.userRepository.delete(user);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return response;
     }
 
 }
--- src/main/java/com/shultzlab/weighttrackerapi/models/User.java
+++ src/main/java/com/shultzlab/weighttrackerapi/models/User.java
@@ -7,6 +7,7 @@ import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.time.Period;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
@@ -38,6 +39,9 @@ public class User {
     @Column
     @Enumerated(EnumType.STRING)
     private Gender gender;
+
+    @Transient
+    private Integer age;
 
     @CreationTimestamp
     @Temporal(TemporalType.TIMESTAMP)
@@ -123,5 +127,13 @@ public class User {
 
     public void setModifiedAt(Date modifiedAt) {
         this.modifiedAt = modifiedAt;
+    }
+
+    public Integer getAge() {
+        return Period.between(this.birthday, LocalDate.now()).getYears();
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
     }
 }
